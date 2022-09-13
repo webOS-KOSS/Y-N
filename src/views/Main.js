ecdc8caed0b0d2cfd4cdfd13a54d3bf6c654dc37
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-09-13 21:44:14 +0900

    Update Main.js

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,24 +2,45 @@ import Button from '@enact/sandstone/Button';
 import React from 'react';
 import './Main.css';
 import { Link } from 'react-router-dom';
+import LS2Request from '@enact/webos/LS2Request';
+
+const bridge = new LS2Request();
 
 const Main = () =>{
+	
 	return (
 		<div className='main'>
 			<div className='sub'>
 				<div className='title'>구성원이 배송품을 수령했습니까?</div>
 				<div className='but'>
-					<Link to='/y'>
-						<Button backgroundOpacity='transparent' size='small'>예</Button>
-					</Link>
-					<Link to='/n'>
-						<Button backgroundOpacity='transparent' size='small'>아니오</Button>
-					</Link>
+						<Button backgroundOpacity='transparent' size='small' onClick={yesServiceStart}>예</Button>
+						<Button backgroundOpacity='transparent' size='small' onClick={noServiceStart}>아니오</Button>
 				</div>
 			</div>
 		</div>
-		
 	)
+}
+
+function yesServiceStart(){
+	var lsRequest = {
+		service:"luna://com.delivery.app.service",
+		method:"yesServiceStart",
+		parameters: {},
+		onSuccess: (msg) => {console.log(msg);},
+		onFailure: (msg) => {console.log(msg);},
+	}
+	bridge.send(lsRequest);
+}
+
+function noServiceStart(){
+	var lsRequest = {
+		service:"luna://com.delivery.app.service",
+		method:"noServiceStart",
+		parameters: {},
+		onSuccess: (msg) => {console.log(msg);},
+		onFailure: (msg) => {console.log(msg);},
+	}
+	bridge.send(lsRequest);
 }
 
 export default Main;
